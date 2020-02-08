4f4984484097e1d6c44e530e44b2d0224b18512c
julita_koss@tlen.pl <julitakoss@github.com>
2020-02-08 11:33:57 +0100

    poprawienie logów

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let a = 12;
 
 if (a > 10){
-    Podana liczba jest  większa od 10;
+    console.log ("Podana liczba jest większa od 10");
 } else {
-    Podana liczba nie jest większa od 10;
+   console.log ("Podana liczba nie jest większa od 10");
 }
 
